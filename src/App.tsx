1d53f3fb45dccb685a6edb2e56e5b8f8bf49d032
jaredbare <jab468@byu.edu>
2023-03-22 18:36:45 -0600

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CollegeBasketballTeams from './CollegeBasketballTeams.json';
+
+class Team extends React.Component {
+  render() {
+    const oneTeam: any = this.props;
+    return (
+      <tr>
+        <td>{oneTeam.school}</td>
+        <td>{oneTeam.name}</td>
+        <td>
+          {oneTeam.city}, {oneTeam.state}
+        </td>
+      </tr>
+    );
+  }
+}
+
+function TeamList() {
+  const teamsInfo = CollegeBasketballTeams.teams;
+  return (
+    <div>
+      {teamsInfo.map((oneTeam) => (
+        <Team {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Jared's Website!!!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+
+      {/* Render the TeamList component and pass in the CollegeBasketballTeams array as a prop */}
+      <TeamList />
     </div>
   );
 }
